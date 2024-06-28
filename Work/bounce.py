6c8c3aa7c88da10cdd9a2354d8a1802d5fb7385e
Sadhaklal <sambit.mukherjee@gmail.com>
2024-06-28 16:08:01 +0530

    Completed "Exercise 1.5: The Bouncing Ball"

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,6 @@
-# bounce.py
-#
-# Exercise 1.5
+height = 100.0
+bounce_number = 1
+while bounce_number <= 10:
+    height = round((3 / 5) * height, 4)
+    print(bounce_number, height)
+    bounce_number += 1
